b9e63ef6c76a63028f6c047e4e7d3d2a813592bd
Ge Will <531sunlight@gmail.com>
2016-09-08 19:53:00 +0800

    美颜+其他滤镜

--- FilterCamera/ViewController.swift
+++ FilterCamera/ViewController.swift
@@ -11,8 +11,12 @@ import UIKit
 class ViewController: UIViewController {
 
     var videoCamera: GPUImageVideoCamera!
-    var filter: YUGPUImageHighPassSkinSmoothingFilter!
-    
+    var movieWriter: GPUImageMovieWriter!
+    var skinFilter: YUGPUImageHighPassSkinSmoothingFilter!
+    var hsbFilter: GPUImageHSBFilter!
+    var curveFilter:GPUImageToneCurveFilter!
+    var filterGroup: GPUImageFilterGroup!
+
     @IBOutlet var previewView: GPUImageView!
 
     override func viewDidAppear(animated: Bool) {
@@ -20,17 +24,60 @@ class ViewController: UIViewController {
 
         videoCamera = GPUImageVideoCamera(sessionPreset: AVCaptureSessionPreset640x480, cameraPosition: .Back)
         videoCamera!.outputImageOrientation = .Portrait;
-        filter = YUGPUImageHighPassSkinSmoothingFilter()
-        filter.amount = 0.7
-        videoCamera?.addTarget(filter)
+        skinFilter = YUGPUImageHighPassSkinSmoothingFilter()
+        skinFilter.amount = 0.7
 
-        filter.addTarget(previewView)
+        hsbFilter = GPUImageHSBFilter()
+        hsbFilter.reset()
+
+        curveFilter = GPUImageToneCurveFilter(ACV: "new_2_fresh_blue")
+        
+        filterGroup = GPUImageFilterGroup()
+        filterGroup.addFilter(skinFilter)
+        filterGroup.addFilter(hsbFilter)
+        filterGroup.addFilter(curveFilter)
+
+        skinFilter.addTarget(hsbFilter)
+        hsbFilter.addTarget(curveFilter)
+        filterGroup.initialFilters = [skinFilter]
+        filterGroup.terminalFilter = curveFilter
+
+        videoCamera?.addTarget(filterGroup)
+
+        filterGroup.addTarget(previewView)
         videoCamera?.startCameraCapture()
     }
 
-    
     @IBAction func filterSliderChanged(sender: UISlider) {
-        filter.amount = CGFloat(sender.value)
+        skinFilter.amount = CGFloat(sender.value)
+    }
+
+    @IBAction func resetHSBButtonClick(sender: UIButton) {
+        hsbFilter.reset()
+    }
+    @IBAction func resetHButtonClick(sender: UIButton) {
+        hsbFilter.rotateHue(0)
+    }
+    @IBAction func resetSButtonClick(sender: UIButton) {
+        hsbFilter.adjustSaturation(50)
+    }
+    @IBAction func resetBButtonClick(sender: UIButton) {
+        hsbFilter.adjustBrightness(50)
+    }
+    @IBAction func hSliderChanged(sender: UISlider) {
+        hsbFilter.rotateHue(sender.value)
+        print(sender.value)
+        print(hsbFilter.description)
+    }
+    @IBAction func sSliderChanged(sender: UISlider) {
+        hsbFilter.adjustSaturation(sender.value)
+        print(sender.value)
+        print(hsbFilter.description)
+    }
+    @IBAction func bSliderChanged(sender: UISlider) {
+        hsbFilter.adjustBrightness(sender.value)
+        print(sender.value)
+        print(hsbFilter.description)
     }
 }
 
